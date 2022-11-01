1ed6767232857cf01485827a21d6ae596fb54728
RedoTV <redotv1@gmail.com>
2022-11-01 19:39:41 +0300

    deleted:    .editorconfig 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   SerialApi/.config/dotnet-tools.json 	new file:   SerialApi/Controllers/LoginController.cs 	modified:   SerialApi/Controllers/SerialController.cs 	new file:   SerialApi/DatabaseContext/AuthorizeContext.cs 	new file:   SerialApi/FormModels/AddSerialForm.cs 	new file:   SerialApi/FormModels/ChangeSerialForm.cs 	new file:   SerialApi/FormModels/LoginForm.cs 	new file:   SerialApi/HelperClasses/AuthOptions.cs 	modified:   SerialApi/Models/Serial.cs 	new file:   SerialApi/Models/User.cs 	modified:   SerialApi/Program.cs 	modified:   SerialApi/Properties/launchSettings.json 	modified:   SerialApi/SerialApi.csproj 	new file:   SerialApi/Services/SerialService.cs 	modified:   SerialApi/appsettings.json 	modified:   SerialApi/doc.xml 	renamed:    SerialApi/DatabaseFiles/SerialInfo.db -> SerialApi/wwwroot/DatabaseFiles/SerialInfo.db 	new file:   SerialApi/wwwroot/DatabaseFiles/UsersInfo.db

---
+++ SerialApi/FormModels/ChangeSerialForm.cs
@@ -0,0 +1,15 @@
+﻿namespace SerialApi.FormModels
+{
+    public class ChangeSerialForm
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Franchise { get; set; }
+        public string? Description { get; set; }
+        public IFormFile? Poster { get; set; }
+        public int? Age { get; set; }
+        public bool? Strict { get; set; }
+        public int? Lenght { get; set; }
+        public string? Kind { get; set; }
+    }
+}
--- SerialApi/Models/Serial.cs
+++ SerialApi/Models/Serial.cs
@@ -1,25 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SerialApi.Models
 {
     public class Serial
     {
-        public Serial(string name, string description, string pathToPoster,string shikiId)
+        public Serial(string name, string description, int shikiId , string franchise , string poster , int? lenght, int? age ,bool? strict , string? kind)
         {
             Name = name;
+            Franchise = franchise;
             Description = description;
-            PathToPoster = pathToPoster;
+            Poster = poster;
             ShikiId = shikiId;
+            Lenght = lenght;
+            Age = age;
+            Strict = strict;
+            Kind = kind;
         }
 
         public int Id { get; set; }
 
         [Required]
         public string Name { get; set; } = null!;
-
+        public string Franchise { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string PathToPoster { get; set; } = null!;
+
         [Required]
-        public string ShikiId { get; set; } = null!;
+        public int ShikiId { get; set; }
+        public string Poster { get; set; } = null!;
+        public int? Lenght { get; set; }
+
+        [DefaultValue(0)]
+        public int? Exist { get; set; }
+
+        [DefaultValue(0d)]
+        public decimal? Rating { get; set; }
+        public string? Kind { get; set; }
+        
+        public int? Age { get; set; }
+        public bool? Strict { get; set; }
     }
 }
